ca94db8744e27f2c3277c939a9329eb40733667d
Dmytro-Kruhlov <hazzy1451@gmail.com>
2023-08-05 12:17:08 +0300

    Add 3 classes name, birthday, phone

---
+++ phone.py
@@ -0,0 +1,16 @@
+import re
+
+class Phone:
+    def __init__(self, value):
+        self.value = value
+        self._value = value
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if not re.match(r'^\d{11}$', value):
+            raise ValueError
+        self._value = value
