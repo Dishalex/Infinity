a22065040f899bcbe4039fc52b5fbe445a8e3891
Dishalex <131618968+Dishalex@users.noreply.github.com>
2023-08-06 17:11:19 +0300

    update get_user_name

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from rich.table import Table
 
 I = 1
 
+
 def address_book_commands():
-    table_address_book = Table(title='\nALL COMMANDS FOR ADDRESS BOOK:\nImportant!!! All entered data must be devided by gap! Phone number must have 10 or 12 digits!\n * - optional paramiters')
+    table_address_book = Table(
+        title='\nALL COMMANDS FOR ADDRESS BOOK:\nImportant!!! All entered data must be devided by gap! Phone number must have 10 or 12 digits!\n * - optional paramiters')
     table_address_book.add_column('COMMAND', justify='left')
     table_address_book.add_column('NAME', justify='left')
     table_address_book.add_column('PHONE NUMBER', justify='letf')
@@ -18,16 +20,19 @@ def address_book_commands():
     table_address_book.add_column('BIRTHDAY', justify='left')
     table_address_book.add_column('DESCRIPTION', justify='left')
     table_address_book.add_row('hello', '-', '-', '-', '-', 'Greeting')
-    table_address_book.add_row('add', 'Any name ', 'Phone number *', 'E-mail', 'Birthday', 'Add new contact')
-    #table.add_row('append', 'Existing name', 'Additional phone number *', '-', 'Append phone number') 
-    #table.add_row('delete', 'Existing name', 'Phone to delete *', '-', 'Delete phone number')
-    #table.add_row('birthday', 'Existing name', '-', 'YYYY-MM-DD', 'Add birthday')
-    #table.add_row('days to birthday', 'Existing name', '-', '-', 'Sow days to birthday')
-    #table.add_row('phone', 'Existing name', '-', '-', 'Getting phone number')
-    #table.add_row('show all / show all + N', '-', '-', '-', 'Getting Address Book/ N - quantity of records on the page')
-    #table.add_row('search + sample', '-', '-', '-', 'searching <<< sumple >>> in address book')
-    table_address_book.add_row('good bye / close / exit', '-', '-', '-', '-', 'Exit')
-    table_address_book.add_row('help', '-', '-', '-', '-', 'Printing table of commands')
+    table_address_book.add_row(
+        'add', 'Any name ', 'Phone number *', 'E-mail', 'Birthday', 'Add new contact')
+    # table.add_row('append', 'Existing name', 'Additional phone number *', '-', 'Append phone number')
+    # table.add_row('delete', 'Existing name', 'Phone to delete *', '-', 'Delete phone number')
+    # table.add_row('birthday', 'Existing name', '-', 'YYYY-MM-DD', 'Add birthday')
+    # table.add_row('days to birthday', 'Existing name', '-', '-', 'Sow days to birthday')
+    # table.add_row('phone', 'Existing name', '-', '-', 'Getting phone number')
+    # table.add_row('show all / show all + N', '-', '-', '-', 'Getting Address Book/ N - quantity of records on the page')
+    # table.add_row('search + sample', '-', '-', '-', 'searching <<< sumple >>> in address book')
+    table_address_book.add_row(
+        'good bye / close / exit', '-', '-', '-', '-', 'Exit')
+    table_address_book.add_row(
+        'help', '-', '-', '-', '-', 'Printing table of commands')
     return table_address_book
 
 
@@ -59,6 +64,7 @@ def input_error(func):
 
 adressbook = AdressBook()
 
+
 @input_error
 def add_record(args):
     if args[0]:
@@ -71,7 +77,7 @@ def add_record(args):
         birthday = Birthday(args[1][0])
         phone = Phone(args[1][1])
         record = Record(name, birthday, phone)
-    
+
     adressbook.add_record(record)
     for rec in adressbook.data.values():
         print(rec)
@@ -80,7 +86,7 @@ def add_record(args):
 @input_error
 def add_p(args):
     name = args[0]
-    
+
     record = adressbook[name]
     if name in adressbook.data:
         phone = Phone(args[1][0])
@@ -95,14 +101,15 @@ def change_p(args):
 
     if name not in adressbook.data:
         return f"You dont have contact with name {name}"
-    
+
     record = adressbook[name]
-    
+
     record.change_phone(old_phone, new_phone)
     for rec in adressbook.data.values():
         print(rec)
     return f"The phone number {old_phone} for contact {name} has been changed to {new_phone}."
-    
+
+
 COMMANDS = {
     add_record: ('add', 'append'),
     change_p: ("change phone", ),
@@ -123,29 +130,21 @@ COMMANDS = {
 def get_user_name(user_info: str) -> tuple:
 
     regex_name = r'[a-zA-ZА-Яа-я]+'
-    user_input_split = user_info.strip().split()
-    name_list = []
+    user_info_list = user_info.strip().split()
+    name = ''
 
-    for i in user_input_split:
-        match_name = re.match(regex_name, i)
-        if match_name:
-            if len(match_name.group()) == len(i):
-                name_list.append(i.capitalize())
-                user_info = user_info[match_name.span()[1]:].strip()
-                user_data = user_info
+    if user_info:
+        while True:
+            word = user_info_list[0]
+            match_name = re.match(regex_name, word)
+            if match_name and len(match_name.group()) == len(word):
+                name = name + word + ' '
+                user_info_list.remove(word)
+                print(user_info_list)
             else:
-                print(f'\nName <<< {i} >>> is not correct! Try again!')
-                user_data = ('', '')
-                return user_data
+                break
 
-    user_data = user_data.split()
-    if len(name_list) >= 1:
-        name = ' '.join(name_list)
-    else:
-        name = ''
-        user_data = []
-
-    return name, user_data
+    return name.strip(), user_info_list
 
 
 def parser(user_input: str):
@@ -161,16 +160,16 @@ def parser(user_input: str):
     user_info = None
     return command, user_info
 
+
 @input_error
 def main():
 
     global I
     if I == 1:
-        #address_book.load_data()
-        print (address_book_commands())
-        print (note_book_commands())
+        # address_book.load_data()
+        print(address_book_commands())
+        print(note_book_commands())
         I += 1
-
 
     while True:
 
@@ -182,8 +181,6 @@ def main():
 
         result = command(user_data)
         print(result)
-
-        
 
 
 if __name__ == "__main__":
