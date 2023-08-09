35d2b1e62aba51d894188080b9c9936b750673a6
Dishalex <131618968+Dishalex@users.noreply.github.com>
2023-08-10 00:06:20 +0300

    Revert "package build with poetry"

--- packaged-with-poetry/Infinity/address_class.py
+++
@@ -1,9 +0,0 @@
-class Address:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self) -> str:
-        return self.value
-
-    def __repr__(self) -> str:
-        return str(self)
